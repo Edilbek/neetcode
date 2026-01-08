1ba07ff31c57c4e87663043f5b64d6ffc219a57b
Edilbek <edil.talantbekov@gmail.com>
2026-01-08 16:07:23 +0600

    refactor sliding_window/count_good_substrings.rb

--- sliding_window/count_good_substrings.rb
+++ sliding_window/count_good_substrings.rb
@@ -1,36 +1,60 @@
 # @param {String} s
 # @return {Integer}
+
+# No refactoring
+# def count_good_substrings(s)
+#     map = {}
+#     count = 0
+#     l = 0
+
+#     s.chars.each_with_index do |char, r|
+#         map[char] = (map[char] || 0) + 1
+#         win_length = r - l + 1
+
+#         if win_length > 3
+#             map[s[l]] -= 1
+#             map.delete(s[l]) if map[s[l]] == 0
+#             l += 1
+
+#             if map.values.sum == 3 && map.values.length == 3
+#                 count += 1
+#             end
+#         elsif map.values.sum == 3 && map.values.length == 3
+#               count += 1
+#         end
+#     end
+
+#     count
+# end
+
+
+# With refactoring
 def count_good_substrings(s)
-    map = {}
-    count = 0
-    l = 0
+  freq = Hash.new(0)
+  count = 0
 
-    s.chars.each_with_index do |char, r|
-        map[char] = (map[char] || 0) + 1
-        win_length = r - l + 1
+  s.chars.each_with_index do |char, r|
+    freq[char] += 1
 
-        if win_length > 3
-            map[s[l]] -= 1
-            map.delete(s[l]) if map[s[l]] == 0
-            l += 1
-
-            if map.values.sum == 3 && map.values.length == 3
-                count += 1
-            end
-        elsif map.values.sum == 3 && map.values.length == 3
-              count += 1
-        end
+    if r >= 3
+      l = r - 3
+      left_char = s[l]
+      freq[left_char] -= 1
+      freq.delete(left_char) if freq[left_char] == 0
     end
 
-    count
+    count += 1 if freq.size == 3
+  end
+
+  count
 end
 
 
 s = 'xyzzaz'
 p count_good_substrings(s)
 
-s = 'aababcabc'
-p count_good_substrings(s)
+# s = 'aababcabc'
+# p count_good_substrings(s)
 
 =begin
 
@@ -41,7 +65,7 @@ p count_good_substrings(s)
 }
 
 l
-    r
+r
 x y z z a z
 0 1 2 3 4 5
 
